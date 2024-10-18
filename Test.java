a8c3505f5ea7898e82e748fbf6ea79b04e4eadbe
Nathan Hinthorne <nathanrh856@gmail.com>
2024-10-18 09:00:06 -0700

    add Test.java

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("TESTING SHA3SHAKE.java\n");
+
+        SHA3SHAKE sha3shake = new SHA3SHAKE();
+        sha3shake.init(256);
+    }
+}
