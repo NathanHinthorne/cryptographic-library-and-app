1755d5a460d93a4567eea27a4a9ec619b61fe9fa
Nathan Hinthorne <nathanrh856@gmail.com>
2024-10-29 09:57:05 -0700

    new test

--- Testing.java
+++ Testing.java
@@ -7,27 +7,26 @@ public class Testing {
                 SHA3SHAKE sha3shake = new SHA3SHAKE();
                 sha3shake.init(256);
 
-                System.out.println("--ORIGINAL STATE MATRIX--");
-                sha3shake.printStateMatrix();
+                // System.out.println("\n--Theta Step Mapping Result--");
+                // sha3shake.stepMapTheta();
+                // sha3shake.printStateMatrix();
 
-                System.out.println("\n--Theta Step Mapping Result--");
-                sha3shake.stepMapTheta();
-                sha3shake.printStateMatrix();
+                // System.out.println("\n--Rho Step Mapping Result--");
+                // sha3shake.stepMapRho();
+                // sha3shake.printStateMatrix();
 
-                System.out.println("\n--Rho Step Mapping Result--");
-                sha3shake.stepMapRho();
-                sha3shake.printStateMatrix();
+                // System.out.println("\n--Pi Step Mapping Result--");
+                // sha3shake.stepMapPi();
+                // sha3shake.printStateMatrix();
 
-                System.out.println("\n--Pi Step Mapping Result--");
-                sha3shake.stepMapPi();
-                sha3shake.printStateMatrix();
+                // System.out.println("\n--Chi Step Mapping Result--");
+                // sha3shake.stepMapChi();
+                // sha3shake.printStateMatrix();
 
-                System.out.println("\n--Chi Step Mapping Result--");
-                sha3shake.stepMapChi();
-                sha3shake.printStateMatrix();
+                byte[] message = "Hello, World!".getBytes();
+                sha3shake.absorb(message);
 
-                System.out.println("\n--Iota Step Mapping Result--");
-                sha3shake.stepMapIota();
+                System.out.println("\n--After Absorb--");
                 sha3shake.printStateMatrix();
         }
 }
