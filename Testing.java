87f8102338b14bb05985a8acf27623067eaa05db
Nathan Hinthorne <nathanrh856@gmail.com>
2024-11-02 11:16:56 -0700

    test absorb

--- Testing.java
+++ Testing.java
@@ -26,7 +26,28 @@ public class Testing {
                 byte[] message = "Hello, World!".getBytes();
                 sha3shake.absorb(message);
 
-                System.out.println("\n--After Absorb--");
-                sha3shake.printStateMatrix();
+                System.out.println("message being absorbed:");
+                for (byte b : message) {
+                        System.out.printf("%02X ", b);
+                }
+
+                System.out.println("\n\nsponge's new input array:");
+                for (byte b : sha3shake.input) {
+                        System.out.printf("%02X ", b);
+                }
+
+                byte[] message2 = "Goodbye, Cruel World...".getBytes();
+                sha3shake.absorb(message2); // Absorb the second message
+
+                System.out.println("\n\nmessage being absorbed:");
+                for (byte b : message2) {
+                        System.out.printf("%02X ", b);
+                }
+
+                System.out.println("\n\nsponge's new input array:");
+                for (byte b : sha3shake.input) {
+                        System.out.printf("%02X ", b);
+                }
+
         }
 }
