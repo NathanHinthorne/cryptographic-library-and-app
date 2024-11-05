214c31b3e2212afe3d1d0ae8e04558453edc7e2f
Nathan Hinthorne <nathanrh856@gmail.com>
2024-11-04 20:03:10 -0800

    remove old code

--- Testing.java
+++ Testing.java
@@ -2,26 +2,10 @@
 public class Testing {
 
         public static void main(String[] args) {
-                System.out.println("TESTING SHA3SHAKE.java\n");
 
-                // SHA3SHAKE sha3shake = new SHA3SHAKE();
-
-                System.out.println("Input Message:");
-                // byte[] message = Testing.hexStringToByteArray(byteString);
                 byte[] message = { (byte) 0xe9 };
-                for (byte b : message) {
-                        System.out.printf("%02X ", b);
-                }
 
-                System.out.println("\n");
                 byte[] out = SHA3SHAKE.SHA3(256, message, null);
-                // byte[] out = SHA3SHAKE.SHAKE(128, message, 20, null);
-                // byte[] out = sha3shake.keccakF(message);
-                // String hexResult = "";
-                // for (byte b : out) {
-                // System.out.printf("%02X ", b);
-                // hexResult += String.format("%02X", b).toLowerCase();
-                // }
         }
 
         public static byte[] hexStringToByteArray(String hexString) {
