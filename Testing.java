ba7246047980c3b53b98e47ae0f720cb36b08ee4
Nathan Hinthorne <nathanrh856@gmail.com>
2024-11-04 20:05:54 -0800

    remove old lines

--- Testing.java
+++ Testing.java
@@ -7,14 +7,4 @@ public class Testing {
 
                 byte[] out = SHA3SHAKE.SHA3(256, message, null);
         }
-
-        public static byte[] hexStringToByteArray(String hexString) {
-                int len = hexString.length();
-                byte[] data = new byte[len / 2];
-                for (int i = 0; i < len; i += 2) {
-                        data[i / 2] = (byte) ((Character.digit(hexString.charAt(i), 16) << 4)
-                                        + Character.digit(hexString.charAt(i + 1), 16));
-                }
-                return data;
-        }
 }
